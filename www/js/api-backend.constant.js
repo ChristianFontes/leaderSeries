49a836686e771cd7ac20c9241ad64e08bbf8a009
ChristianFontes <christianfontes82@gmail.com>
2016-05-16 13:30:09 -0430

    change api backend

--- www/js/api-backend.constant.js
+++ www/js/api-backend.constant.js
@@ -3,7 +3,8 @@
    
    angular.module('leaderSeries')
         .constant('apiBackend', {
-            URL: 'http://localhost:1337' //local sails backend
+        	URL: 'https://leaderseriesbackend.herokuapp.com' //remote sails backend
+            //URL: 'http://localhost:1337' //local sails backend
             //URL: 'http://192.168.43.161:1337' //local sails backend 
         });
 })();
